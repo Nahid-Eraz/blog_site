71e029b434039bc28dc654430bcc0e745f3817fc
Nahid Eraz <nahidulislameraz@gmail.com>
2021-10-16 21:31:00 +0600

    navbar with base pages done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './pages/About';
+import ArticlesList from './pages/ArticlesList';
+import Article from './pages/Article';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="container">
+    <Router>
+      <Navbar></Navbar>
+      <div className='max-w-screen-md mx-auto pt-20'>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/articles-list">
+            <ArticlesList></ArticlesList>
+          </Route>
+          <Route path="/article">
+            <Article></Article>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
     
-    </div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="mb-20">
+            <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6  text-gray-900'>Hello, Welcome to my blog</h1>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Welcome to my blog, Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, temporibus. Hic, explicabo velit? Laudantium aspernatur quaerat a exercitationem repellendus iure at praesentium non. Beatae consequuntur ex adipisci sed eum tempore!
+            </p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, animi fuga! Velit, non repellendus. Quidem commodi laborum doloribus porro minima. Adipisci praesentium cumque asperiores odit enim soluta, vero vel incidunt tempora deleniti tempore, esse, tenetur et odio ipsa ratione delectus!
+            </p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quam esse dolorum nihil deserunt reprehenderit, cum consequatur quas accusamus. Culpa!
+            </p>
+        </div>
+    );
+};
+
+export default Home;
